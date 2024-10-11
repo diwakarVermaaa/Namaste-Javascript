5715597114d7e31dd6646bffa14733ef099188bc
divakrrr <88948065+diwakarVermaaa@users.noreply.github.com>
2024-10-11 23:48:17 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,24 @@
+const cart = ["shoes", "pants", "kurta"];
+
+// Below two functions are asynchronous and dependent on each other
+const orderId = createOrder(cart);
+proceedToPayment(orderId);
+
+// with Callback (Before Promise)
+// Below here, it is the responsibility of createOrder function to first create the order then call the callback function
+createOrder(cart, function () {
+  proceedToPayment(orderId);
+});
+
+const cart = ["shoes", "pants", "kurta"];
+
+const promiseRef = createOrder(cart);
+// this promiseRef has access to `then`
+
+// {data: undefined}
+// Initially it will be undefined so below code won't trigger
+// After some time, when execution has finished and promiseRef has the data then automatically the below line will get triggered.
+
+promiseRef.then(function () {
+  proceedToPayment(orderId);
+});
