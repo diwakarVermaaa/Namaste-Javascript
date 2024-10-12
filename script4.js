d9ebcdc90dfd5ff5e2cff5bf60c0799b7acf4b1b
divakrrr <88948065+diwakarVermaaa@users.noreply.github.com>
2024-10-12 22:50:46 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,108 @@
+const cart = ["shoes", "pants", "kurta"];
+
+const promise = createOrder(cart); // orderId
+// ❓ What will be printed in below line?
+// It prints Promise {<pending>}, but why?
+// Because above createOrder is going to take sometime to get resolved, so pending state. But once the promise is resolved, `.then` would be executed for callback.
+console.log(promise);
+
+promise.then(function (orderId) {
+  proceedToPayment(orderId);
+});
+
+function createOrder(cart) {
+  const promise = new Promise(function (resolve, reject) {
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not Valid");
+      reject(err);
+    }
+    const orderId = "12345";
+    if (orderId) {
+      resolve(orderId);
+    }
+  });
+  return promise;
+}
+
+//Using catch
+
+const cart = ["shoes", "pants", "kurta"];
+
+const promise = createOrder(cart); // orderId
+
+// Here we are consuming Promise and will try to catch promise error
+promise
+  .then(function (orderId) {
+    // ✅ success aka resolved promise handling
+    proceedToPayment(orderId);
+  })
+  .catch(function (err) {
+    // ⚠️ failure aka reject handling
+    console.log(err);
+  });
+
+// Here we are creating Promise
+function createOrder(cart) {
+  const promise = new Promise(function (resolve, reject) {
+    // Assume below `validateCart` return false then the promise will be rejected
+    // And then our browser is going to throw the error.
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not Valid");
+      reject(err);
+    }
+    const orderId = "12345";
+    if (orderId) {
+      resolve(orderId);
+    }
+  });
+  return promise;
+}
+
+//From here we'll say promise chaining
+
+const cart = ["shoes", "pants", "kurta"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    // ✅ success aka resolved promise handling
+    // 💡 we have return data or promise so that we can keep chaining the promises, here we are returning data
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    // Promise chaining
+    // 💡 we will make sure that `proceedToPayment` returns a promise too
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    // from above, `proceedToPayment` is returning a promise so we can consume using `.then`
+    console.log(paymentInfo);
+  })
+  .catch(function (err) {
+    // ⚠️ failure aka reject handling
+    console.log(err);
+  });
+
+// Here we are creating Promise
+function createOrder(cart) {
+  const promise = new Promise(function (resolve, reject) {
+    // Assume below `validateCart` return false then the promise will be rejected
+    // And then our browser is going to throw the error.
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not Valid");
+      reject(err);
+    }
+    const orderId = "12345";
+    if (orderId) {
+      resolve(orderId);
+    }
+  });
+  return promise;
+}
+
+function proceedToPayment(cart) {
+  return new Promise(function (resolve, reject) {
+    // For time being, we are simply `resolving` promise
+    resolve("Payment Successful");
+  });
+}
